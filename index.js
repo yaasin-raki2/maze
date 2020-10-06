ee3dff7353857b6f954bebc14f42b9aa95f4a937
Yassine <yassin23raki@gmail.com>
2020-10-06 11:46:57 +0100

    setting up the maze logic

--- index.js
+++ index.js
@@ -36,4 +36,78 @@ const verticals = Array(cells).fill(null).map( () => Array(cells-1).fill(false))
 
 const horizentals = Array(cells-1).fill(null).map( () => Array(cells).fill(false));
 
-console.log(grid);
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const shuffle = (arr) =>  {
+    let counter = arr.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp
+    }
+
+    return arr;
+}
+
+const stepThroughCell = (row, column) => {
+    // If I have visited the cell at [row, column] then return
+    if(grid[row][column]) {
+        return;
+    }
+
+    // Mark this cell as being visited
+    grid[row][column] = true;
+
+    // Asseemble randomly ordered-list of neighbors
+    const neighbors = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
+    ]);
+
+    // For Each neighbor ...
+    for (let neighbor of neighbors) {
+
+        const [nextRow, nextColumn, direction] = neighbor; 
+
+        // See if that neighbor is out of bounds
+        if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells ) {
+            continue;
+        }
+
+        // If I visited that neighbor, continue to the next neighbor
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        }
+
+        // Remove a wall from either horizentals or verticals
+        if (direction === 'left') {
+            verticals[row][column - 1] = true;
+
+        } else if (direction === 'right') {
+            verticals[row][column] = true;
+
+        } else if (direction === 'up') {
+            horizentals[row - 1][column] = true;
+
+        } else if (direction === 'down') {
+            horizentals[row][column] = true;
+        }
+
+        stepThroughCell(nextRow, nextRow);
+    }
+
+    // Visit that next cell
+
+}
+
+stepThroughCell(startRow, startColumn);
+
+console.log(verticals, horizentals);
